6474334b19cc8705b6b3b3a18975330f0194930f
Scott Hiett <sodex.gaming@gmail.com>
2022-01-31 12:08:45 +0000

    TypeScript API updates

--- api-typescript/src/index.ts
+++ api-typescript/src/index.ts
@@ -1,36 +1,3 @@
-import {JobClient} from "./clients/job-client";
-
 export * from "./types";
-
-(async () => {
-  const client = new JobClient("http://localhost:8080");
-
-  // Method 1
-  // const job1 = await client.createJob({
-  //   time: {
-  //     type: "REPEATING",
-  //     firstRunUnix: Date.now(),
-  //     iterations: 10,
-  //     interval: 60,
-  //   },
-  //   action: {
-  //     type: "HTTP",
-  //     url: "https://google.com"
-  //   },
-  //   payload: {
-  //     userId: 234234
-  //   }
-  // });
-
-  // Method 2
-  const job2 = await client.builder()
-    .repeating(60, 10)
-    .toHttp("https://google.com") // LOL.. left a discord webhook url in there! Deleted it
-    .runFirstAt(new Date())
-    .withPayload({
-      content: "I am a scheduled job"
-    })
-    .create();
-
-  console.log(job2);
-})();
+export * from "./clients";
+export * from "./builders";
