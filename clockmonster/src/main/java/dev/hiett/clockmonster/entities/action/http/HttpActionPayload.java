1f679aefd2fce79bbafc796c8357da5aeaa8b859
Scott Hiett <sodex.gaming@gmail.com>
2022-09-13 17:07:50 +0100

    HTTP action now supports webhook signing secrets

--- clockmonster/src/main/java/dev/hiett/clockmonster/entities/action/http/HttpActionPayload.java
+++ clockmonster/src/main/java/dev/hiett/clockmonster/entities/action/http/HttpActionPayload.java
@@ -1,5 +1,6 @@
 package dev.hiett.clockmonster.entities.action.http;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.hiett.clockmonster.entities.action.ActionPayload;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.hibernate.validator.constraints.URL;
@@ -14,12 +15,25 @@ public class HttpActionPayload implements ActionPayload {
     @URL
     private final String url;
 
-    @ConstructorProperties({"http"})
-    public HttpActionPayload(String url) {
+    // Can be null to disable signing
+    private final String signingSecret;
+
+    @ConstructorProperties({"http", "signingSecret"})
+    public HttpActionPayload(String url, String signingSecret) {
         this.url = url;
+        this.signingSecret = signingSecret;
     }
 
     public String getUrl() {
         return url;
+    }
+
+    public String getSigningSecret() {
+        return signingSecret;
+    }
+
+    @JsonIgnore
+    public boolean signingEnabled() {
+        return getSigningSecret() != null;
     }
 }
